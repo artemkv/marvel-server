90c0fab1014f01de3bc4fe423f137fcbb48ac8a1
artemkv <artemkv@gmail.com>
2019-02-24 16:22:49 +0100

    Clean up imports

--- src/main/java/net/artemkv/marvelserver/MarvelServerApplication.java
+++ src/main/java/net/artemkv/marvelserver/MarvelServerApplication.java
@@ -2,7 +2,6 @@ package net.artemkv.marvelserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
